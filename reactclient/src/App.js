c20af1b6f5a0515e3be13c5c22dba4ba730d13c8
Dzmitry <popovdv096@gmail.com>
2023-05-08 21:30:06 +0200

    cleaned react app and added bootstrap;

--- reactclient/src/App.js
+++ reactclient/src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row min-vh-100">
+        <div className="col d-flex flex-column justify-content-center align-items-center">
+          <h1>Hello World!</h1>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
